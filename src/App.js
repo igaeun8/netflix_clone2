59c53d3e3509cbaeae080928b0a4fabd4079b72f
igaeun8 <igaeun8@gmail.com>
2025-12-05 16:17:17 +0900

    feat(router): React Router 설정 및 기본 구조 완성
    - React Router DOM 설정
    - 모든 라우트 경로 구성
    - Context 및 Hooks 폴더 구조 준비

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ROUTES } from './constants/routes';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
+import Wishlist from './pages/Wishlist';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SIGNIN} element={<SignIn />} />
+        <Route path={ROUTES.POPULAR} element={<Popular />} />
+        <Route path={ROUTES.SEARCH} element={<Search />} />
+        <Route path={ROUTES.WISHLIST} element={<Wishlist />} />
+      </Routes>
+    </Router>
   );
 }
 
